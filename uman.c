13130d2f722a0c5596eb04fa9c7d395cba06dcd7
Ahmad Fatoum <ahmad@a3f.at>
2018-01-06 12:57:21 +0100

    Add (untested) uman_enslave in debugfs_set_slave

--- uman.c
+++ uman.c
@@ -800,17 +800,17 @@ static ssize_t debugfs_get_slave(struct file *file, char __user *buff, size_t co
     struct slave *slave = uman_slave(uman);
 
     if (!slave)
-        return 0;
+        return -EAGAIN;
 
     return simple_read_from_buffer(buff, count, offset, slave->dev->name, strlen(slave->dev->name));
 }
 static ssize_t debugfs_set_slave(struct file *file, const char __user *buff, size_t count, loff_t *offset)
 {
     struct net_device *uman_dev = file->f_inode->i_private;
-    struct uman *uman = netdev_priv(uman_dev);
     struct net_device *slave_dev;
     char ifname[IFNAMSIZ+1];
     ssize_t nulpos;
+    int result;
     ssize_t ret = simple_write_to_buffer(ifname, sizeof ifname - 1, offset, buff, count);
     if (ret <= 0)
         return ret;
@@ -828,7 +828,8 @@ static ssize_t debugfs_set_slave(struct file *file, const char __user *buff, siz
 
     printk(DRV_NAME ": (%p) You want to enslave %s@%p (%s)?\n", uman_dev, ifname, slave_dev, slave_dev->name);
 
-    uman->slave.dev = slave_dev;
+    if ((result = uman_enslave(uman_dev, slave_dev)))
+        return result;
 
     return ret;
 }
